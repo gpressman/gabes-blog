1b3ae1b532e5cded607f51aa0b349c3da20c9337
Gabe Pressman <pressman4747@gmail.com>
2015-01-15 17:21:29 -0500

    advanced

--- object.rb
+++ object.rb
@@ -31,10 +31,19 @@ class Post
 	end
 end
 
+class Sponsored <Post
+
+def to_s
+	return "*******#{@title}*******\n*******************************************#\n#{@text} date: \n#{Time.new}\n\n-------------------------------------\n"
+	
+end
+
+
+end
 
 gabe=Blog.new
 post1=Post.new("Blog post number 1", "hella content")
-post2=Post.new("Second blog post", "more content")
+post2=Sponsored.new("Second blog post", "more content")
 post3=Post.new("Third", "finished")
 gabe.add(post1)
 gabe.add(post2)
